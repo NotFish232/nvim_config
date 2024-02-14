4375d381927fb04a77e21af157feabaf683bf507
Justin Lee <2025jlee@tjhsst.edu>
2024-02-14 11:54:53 -0500

    fix weird telescope thing

--- lua/plugins/editor/telescope-undo.lua
+++ lua/plugins/editor/telescope-undo.lua
@@ -7,17 +7,6 @@ return {
             dependencies = { "nvim-lua/plenary.nvim" },
         },
     },
-    opts = {
-        extensions = {
-            undo = {
-                side_by_side = true,
-                layout_strategy = "vertical",
-                layout_config = {
-                    preview_height = 0.8,
-                },
-            },
-        },
-    },
     config = function(_, opts)
         -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
         -- configs for us. We won't use data, as everything is in it's own namespace (telescope
