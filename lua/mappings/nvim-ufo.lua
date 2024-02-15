9f780fa3a5746a7024beda6022cbaf6e8d3f2ed6
Justin Lee <2025jlee@tjhsst.edu>
2024-02-14 19:35:38 -0500

    dont wait to close all folds

--- lua/mappings/nvim-ufo.lua
+++ lua/mappings/nvim-ufo.lua
@@ -1,4 +1,4 @@
 -- toggle folds using <leader>f
 vim.keymap.set({ 'n', 'v' }, '<leader>f', 'za')
-vim.keymap.set({ 'n', 'v' }, '<leader>ff', require('ufo').closeAllFolds)
-vim.keymap.set({ 'n', 'v' }, '<leader>F', require('ufo').openAllFolds)
+vim.keymap.set({ 'n', 'v' }, '<leader>F', require('ufo').closeAllFolds)
+vim.keymap.set({ 'n', 'v' }, '<leader>FF', require('ufo').openAllFolds)
