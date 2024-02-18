c9da6c9e256d93bea0c27be8879cabaeb69b5707
Justin Lee <2025jlee@tjhsst.edu>
2024-02-18 11:17:58 -0500

    add vim as a defined global for lua lsp

--- init.lua
+++ init.lua
@@ -156,8 +156,9 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+      diagnostics = {
+        globals = { 'vim' }
+      },
     },
   },
 }
