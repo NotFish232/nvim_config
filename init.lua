ec58c1ab3cfe9e2b71a6d2f44e87c13479ddb30a
Justin Lee <2025jlee@tjhsst.edu>
2024-02-18 21:33:33 -0500

    remove netrw again

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
