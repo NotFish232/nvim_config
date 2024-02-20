09a51dfab9a04852eefd1cd85cc0e6fa84892142
Justin Lee <2025jlee@tjhsst.edu>
2024-02-19 19:46:00 -0500

    add keybinds for multi terminal with ToggleTerm

--- lua/mappings/editor/toggleterm.lua
+++ lua/mappings/editor/toggleterm.lua
@@ -1,3 +1,7 @@
 -- t + horizontal / vertical terminal
 vim.keymap.set('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>tv', '<cmd>ToggleTerm direction=vertical<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tnh', '<cmd>2ToggleTerm direction=horizontal<cr>', { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>tnv', '<cmd>2ToggleTerm direction=vertical<cr>', { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>tmh', '<cmd>3ToggleTerm direction=horizontal<cr>', { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>tmv', '<cmd>3ToggleTerm direction=vertical<cr>', { noremap = true, silent = true})
